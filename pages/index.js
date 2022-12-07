994c44265055406a19eaa13ac9a3f09578089247
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-12-07 18:02:26 -0300

    feat: create news pages

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Layout from "../components/layout/Layout";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "Paris React Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Louvre_Museum_Wikimedia_Commons.jpg/1280px-Louvre_Museum_Wikimedia_Commons.jpg",
+    address: "Some Street 5, 123 - Paris, France",
+    description: "Sample description",
+  },
+  {
+    id: "m1",
+    title: "Prague Vuejs Meetup",
+    image:
+      "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRtDk1qH7TEFjzO9UvReZt-FIGe4E3zOQKCDSS_gCOrEj1oVPytahfV2pW8rMeWUOuv",
+    address: "Some Street 5, 123 - Prague, Czech Republic",
+    description: "Sample description",
+  },
+];
+
+export default function Home() {
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
+}
---
+++ pages/new-meetup.js
@@ -0,0 +1,9 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+export default function NewMeetup() {
+  const addMeetupHandler = (meetupData) => {
+    console.log(meetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
