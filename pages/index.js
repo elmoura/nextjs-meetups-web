1f05b8eca776a292ce549a607f9460b5c5ff50bd
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-12-08 17:44:39 -0300

    feat: integrate homepage with MongoDB data in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,24 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "Paris React Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Louvre_Museum_Wikimedia_Commons.jpg/1280px-Louvre_Museum_Wikimedia_Commons.jpg",
-    address: "Some Street 5, 123 - Paris, France",
-    description: "Sample description",
-  },
-  {
-    id: "m2",
-    title: "Prague Vuejs Meetup",
-    image:
-      "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRtDk1qH7TEFjzO9UvReZt-FIGe4E3zOQKCDSS_gCOrEj1oVPytahfV2pW8rMeWUOuv",
-    address: "Some Street 5, 123 - Prague, Czech Republic",
-    description: "Sample description",
-  },
-];
 
 // Static Site Generation (SSG) approach
 // This function only runs during the build process
 export async function getStaticProps() {
-  // code that runs on a Node.js server
-  // fetch data from API, databases, filesystem, etc.
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
+  const meetupsCollection = client.db().collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
 
   return {
     // [required key] passes props to the page components
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        // description: meetup.description,
+      })),
     },
     // [optional key] adds Incremental Static Generation (ISG), which updates
     //    the data passed in props. The revalidation time is passed in seconds.
