1768e70eaf57d7182ee40d23749334bc89e12733
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-12-08 17:24:09 -0300

    feat: add POST - /api/meetups route to create new meetups

---
+++ pages/api/meetups.js
@@ -0,0 +1,34 @@
+// Path: /api/meetup
+
+import { MongoClient } from "mongodb";
+
+export default async function handler(request, response) {
+  if (request.method === "POST") {
+    try {
+      const data = request.body;
+
+      const { title, image, address, description } = data;
+
+      const client = await MongoClient.connect(process.env.MONGODB_URL);
+      const db = await client.db();
+
+      const meetupsCollection = db.collection("meetups");
+      const result = await meetupsCollection.insertOne({
+        title,
+        description,
+        image,
+        address,
+      });
+
+      client.close();
+
+      return response.status(201).json(result);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        message: "An error ocurred creating a meetup",
+        error,
+      });
+    }
+  }
+}
--- pages/api/new-meetup.js
+++
@@ -1,9 +0,0 @@
-// Path: /api/new-meetup
-
-export default function handler(request, response) {
-  if (request.method === "POST") {
-    const data = request.body;
-
-    const { title, image, address, description } = data;
-  }
-}
