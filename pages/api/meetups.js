d4581360a7e2cd706c505f74476774c97ab29627
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-12-08 17:42:55 -0300

    refac: create separate function for meetup creation

--- pages/api/meetups.js
+++ pages/api/meetups.js
@@ -4,31 +4,35 @@ import { MongoClient } from "mongodb";
 
 export default async function handler(request, response) {
   if (request.method === "POST") {
-    try {
-      const data = request.body;
-
-      const { title, image, address, description } = data;
-
-      const client = await MongoClient.connect(process.env.MONGODB_URL);
-      const db = await client.db();
-
-      const meetupsCollection = db.collection("meetups");
-      const result = await meetupsCollection.insertOne({
-        title,
-        description,
-        image,
-        address,
-      });
-
-      client.close();
-
-      return response.status(201).json(result);
-    } catch (error) {
-      console.error(error);
-      return response.status(500).json({
-        message: "An error ocurred creating a meetup",
-        error,
-      });
-    }
+    return createMeetup(request, response);
   }
 }
+
+const createMeetup = async (request, response) => {
+  try {
+    const data = request.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(process.env.MONGODB_URL);
+    const db = await client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne({
+      title,
+      description,
+      image,
+      address,
+    });
+
+    client.close();
+
+    return response.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      message: "An error ocurred creating a meetup",
+      error,
+    });
+  }
+};
